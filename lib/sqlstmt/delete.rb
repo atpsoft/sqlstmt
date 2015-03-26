079a584aba37228c9521e74590a1d4e7d67e07ea
Makani Mason <gitauthor@pqmland.com>
2015-03-26 14:27:35 -0600

    improve multi table delete support, bump version

--- lib/sqlstmt/delete.rb
+++ lib/sqlstmt/delete.rb
@@ -3,14 +3,15 @@ require 'sqlstmt/from_query'
 module SqlStmt
 
 class Delete < FromQuery
+  force_deep_copy :from_tables
+
   def initialize
     super
-    @from_table = nil
+    @from_tables = []
   end
 
   def from(table)
-    @from_table = table
-    @tables.push(table)
+    @from_tables << table
     self
   end
 
@@ -18,14 +19,14 @@ private
   def verify_minimum_requirements
     super
     combined_table_count = @tables.size + @joins.size
-    raise SqlStmt::Error, "unable to build sql - must call :from when including multiple tables" if @from_table.nil? && (combined_table_count > 1)
+    raise SqlStmt::Error, "unable to build sql - must call :from when including multiple tables" if @from_tables.empty? && (combined_table_count > 1)
   end
 
   def build_stmt
-    if @from_table
-      table_clause = " #@from_table"
-    else
+    if @from_tables.empty?
       table_clause = ''
+    else
+      table_clause = ' ' + @from_tables.join(',')
     end
     "DELETE#{table_clause}#{build_from_clause}"
   end
--- test/delete.dt.rb
+++ test/delete.dt.rb
@@ -8,7 +8,8 @@ class TestDelete < DohTest::TestGroup
   end
 
   def test_simple
-    assert_equal('DELETE target FROM target,other_table', Delete.new.from('target').table('other_table').no_where.to_s)
+    assert_equal('DELETE t FROM target t,other_table o', Delete.new.from('t').table('target t').table('other_table o').no_where.to_s)
+    assert_equal('DELETE t,o FROM t JOIN o ON t.id=o.id', Delete.new.from('t').from('o').table('t').join('o','t.id=o.id').no_where.to_s)
     assert_equal('DELETE FROM target', Delete.new.table('target').no_where.to_s)
     assert_equal('DELETE FROM target WHERE target_id = 1', Delete.new.table('target').where('target_id = 1').to_s)
   end
