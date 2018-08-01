2b2b228e25d66185ac0e2508d68199feb6c726f2
Makani Mason <gitauthor@pqmland.com>
2018-08-01 17:14:47 -0600

    switch to using SqlData struct

--- lib/sqlstmt/build.rb
+++ lib/sqlstmt/build.rb
@@ -10,107 +10,107 @@ class SqlStmt
   ###### the remainder of the methods are for verifying and building the completed statement string
 
   def verify_minimum_requirements
-    if !@stmt_type
+    if !@data.stmt_type
       raise SqlStmtError, "unable to build sql - must call :select, :update, :insert or :delete to specify statement type"
     end
-    if @tables.empty?
+    if @data.tables.empty?
       raise SqlStmtError, "unable to build sql - must call :table"
     end
 
-    if (@where_behavior == :require) && @wheres.empty?
+    if (@data.where_behavior == :require) && @data.wheres.empty?
       raise SqlStmtError, "unable to build sql - must call :where, :no_where, or :optional_where"
-    elsif (@where_behavior == :exclude) && !@wheres.empty?
+    elsif (@data.where_behavior == :exclude) && !@data.wheres.empty?
       raise SqlStmtError, "unable to build sql - :where and :no_where must not both be called, consider :optional_where instead"
     end
 
-    if @stmt_type == 'select'
-      raise SqlStmtError, "unable to build sql - must call :get" if @fields.empty?
-      raise SqlStmtError, "unable to build sql - must not call :set" if !@values.empty?
+    if @data.stmt_type == 'select'
+      raise SqlStmtError, "unable to build sql - must call :get" if @data.fields.empty?
+      raise SqlStmtError, "unable to build sql - must not call :set" if !@data.values.empty?
     else
-      raise SqlStmtError, "unable to build sql - must not call :get" if @called_get
+      raise SqlStmtError, "unable to build sql - must not call :get" if @data.called_get
     end
 
-    if ['update','insert'].include?(@stmt_type)
-      raise SqlStmtError, "unable to build sql - must call :set or :setq" if @values.empty?
-      raise SqlStmtError, "unable to build sql - must not call :get" if @called_get
+    if ['update','insert'].include?(@data.stmt_type)
+      raise SqlStmtError, "unable to build sql - must call :set or :setq" if @data.values.empty?
+      raise SqlStmtError, "unable to build sql - must not call :get" if @data.called_get
     end
 
-    if @stmt_type == 'insert'
-      raise SqlStmtError, "unable to build sql - must call :into" if @into_table.nil?
+    if @data.stmt_type == 'insert'
+      raise SqlStmtError, "unable to build sql - must call :into" if @data.into_table.nil?
     end
 
-    if @stmt_type == 'delete'
-      raise SqlStmtError, "unable to build sql - must not call :get or :set" if !@fields.empty?
-      if @tables_to_delete.empty? && ((@tables.size + @joins.size) > 1)
+    if @data.stmt_type == 'delete'
+      raise SqlStmtError, "unable to build sql - must not call :get or :set" if !@data.fields.empty?
+      if @data.tables_to_delete.empty? && ((@data.tables.size + @data.joins.size) > 1)
         raise SqlStmtError, "unable to build sql - must specify tables to delete when including multiple tables"
       end
     end
   end
 
   def build_stmt
-    method_name = "build_stmt_#{@stmt_type}"
+    method_name = "build_stmt_#{@data.stmt_type}"
     return send(method_name)
   end
 
   def build_stmt_select
-    straight_join_str = @straight_join ? 'STRAIGHT_JOIN ' : ''
-    distinct_str = @distinct ? 'DISTINCT ' : ''
-    select_str = @fields.join(',')
-    return "SELECT #{straight_join_str}#{distinct_str}#{select_str}#{build_from_clause}#{@outfile}"
+    straight_join_str = @data.straight_join ? 'STRAIGHT_JOIN ' : ''
+    distinct_str = @data.distinct ? 'DISTINCT ' : ''
+    select_str = @data.fields.join(',')
+    return "SELECT #{straight_join_str}#{distinct_str}#{select_str}#{build_from_clause}#{@data.outfile}"
   end
 
   def build_stmt_update
-    limit_clause = simple_clause('LIMIT', @limit)
+    limit_clause = simple_clause('LIMIT', @data.limit)
     return "UPDATE #{build_table_list}#{build_join_clause} SET #{build_set_clause}#{build_where_clause}#{limit_clause}"
   end
 
   def build_stmt_insert
-    if !@fields.empty? && !@rows.empty?
+    if !@data.fields.empty? && !@data.rows.empty?
       raise "unable to use INSERT SELECT and INSERT VALUES together, may only call :set or :add_row, but not both"
     end
 
-    keyword = @replace ? 'REPLACE' : 'INSERT'
-    value_list = @values.join(',')
-    start_str = "#{keyword} #{@ignore}INTO #{@into_table} "
-    if !@fields.empty?
-      field_list = @fields.join(',')
+    keyword = @data.replace ? 'REPLACE' : 'INSERT'
+    value_list = @data.values.join(',')
+    start_str = "#{keyword} #{@data.ignore}INTO #{@data.into_table} "
+    if !@data.fields.empty?
+      field_list = @data.fields.join(',')
       start_str += "(#{field_list}) "
     end
 
-    if @rows.empty?
-      distinct_str = @distinct ? 'DISTINCT ' : ''
+    if @data.rows.empty?
+      distinct_str = @data.distinct ? 'DISTINCT ' : ''
       return "#{start_str}SELECT #{distinct_str}#{value_list}#{build_from_clause}"
     else
-      raise "DISTINCT not supported when inserting values" if @distinct
+      raise "DISTINCT not supported when inserting values" if @data.distinct
       return "#{start_str}VALUES (#{value_list})"
     end
   end
 
   def build_stmt_delete
-    if @tables_to_delete.empty?
+    if @data.tables_to_delete.empty?
       table_clause = ''
     else
-      table_clause = ' ' + @tables_to_delete.join(',')
+      table_clause = ' ' + @data.tables_to_delete.join(',')
     end
     return "DELETE#{table_clause}#{build_from_clause}"
   end
 
   def build_from_clause
     join_clause = build_join_clause
-    group_clause = simple_clause('GROUP BY', @group_by)
-    if @with_rollup
+    group_clause = simple_clause('GROUP BY', @data.group_by)
+    if @data.with_rollup
       group_clause += ' WITH ROLLUP'
     end
-    order_clause = simple_clause('ORDER BY', @order_by)
-    limit_clause = simple_clause('LIMIT', @limit)
-    having_clause = @having.empty? ? '' : " HAVING #{@having.join(' AND ')}"
+    order_clause = simple_clause('ORDER BY', @data.order_by)
+    limit_clause = simple_clause('LIMIT', @data.limit)
+    having_clause = @data.having.empty? ? '' : " HAVING #{@data.having.join(' AND ')}"
     return " FROM #{build_table_list}#{join_clause}#{build_where_clause}#{group_clause}#{having_clause}#{order_clause}#{limit_clause}"
   end
 
   def build_set_clause
     set_exprs = []
-    @fields.each_with_index do |field, index|
-      set_exprs << "#{field} = #{@values[index]}"
+    @data.fields.each_with_index do |field, index|
+      set_exprs << "#{field} = #{@data.values[index]}"
     end
     return set_exprs.join(', ')
   end
@@ -123,7 +123,7 @@ class SqlStmt
   end
 
   def build_table_list
-    return @tables.map {|table| table_to_str(table) }.join(',')
+    return @data.tables.map {|table| table_to_str(table) }.join(',')
   end
 
   def simple_clause(keywords, value)
@@ -131,14 +131,14 @@ class SqlStmt
   end
 
   def build_join_clause
-    if @joins.empty?
+    if @data.joins.empty?
       return ''
     else
-      return ' ' + @joins.map {|ary| ary.join(' ')}.uniq.join(' ')
+      return ' ' + @data.joins.map {|ary| ary.join(' ')}.uniq.join(' ')
     end
   end
 
   def build_where_clause
-    return @wheres.empty? ? '' : " WHERE #{@wheres.join(' AND ')}"
+    return @data.wheres.empty? ? '' : " WHERE #{@data.wheres.join(' AND ')}"
   end
 end
--- lib/sqlstmt/sqlstmt.rb
+++ lib/sqlstmt/sqlstmt.rb
@@ -12,68 +12,55 @@ require 'sqlstmt/error'
 # also, looking to the future of supporting other dialects of SQL, I think the same will be true there
 # meaning, we don't the choice of SQL dialect to be allowed at any time
 class SqlStmt
-  attr_reader :fields, :tables, :joins, :wheres
-
   def initialize
-    @stmt_type = nil
-    @tables = []
-    @joins = []
-    @wheres = []
-    @where_behavior = :require
-    @fields = []
-    @values = []
-    @group_by = nil
-    @order_by = nil
-    @limit = nil
-    @having = []
-    @into_table = nil
-    @rows = []
-    @tables_to_delete = []
-    @distinct = nil
-    @straight_join = nil
-    @replace = nil
-    @ignore = ''
-    @outfile = ''
-    @with_rollup = nil
-    # track this explicitly to guarantee get is not used with non-select statements
-    @called_get = false
+    @data = SqlStmtLib::SqlData.new
   end
 
-  def initialize_copy(orig)
-    super
-    @tables = @tables.dup
-    @joins = @joins.dup
-    @wheres = @wheres.dup
-    @fields = @fields.dup
-    @values = @values.dup
-    @having = @having.dup
-    @rows = @rows.dup
+  def initialize_copy(_orig)
+    @data = @data.dup
+  end
+
+  ###### temporary transition methods
+  def fields
+    return @data.fields
+  end
+
+  def tables
+    return @data.tables
+  end
+
+  def joins
+    return @data.joins
+  end
+
+  def wheres
+    return @data.wheres
   end
 
   ###### pick statement type
 
   def select
     ensure_no_statement_type
-    @stmt_type = 'select'
+    @data.stmt_type = 'select'
     return self
   end
 
   def update
     ensure_no_statement_type
-    @stmt_type = 'update'
+    @data.stmt_type = 'update'
     return self
   end
 
   def insert
     ensure_no_statement_type
-    @stmt_type = 'insert'
+    @data.stmt_type = 'insert'
     return self
   end
 
   def delete(*tables)
     ensure_no_statement_type
-    @stmt_type = 'delete'
-    @tables_to_delete = tables
+    @data.stmt_type = 'delete'
+    @data.tables_to_delete = tables
     return self
   end
 
@@ -82,7 +69,7 @@ class SqlStmt
   def table(table_str, use_index = nil)
     parts = table_str.split(' ')
     table_obj = SqlStmtLib::SqlTable.new(table_str, parts[0], parts[1], use_index)
-    @tables << table_obj
+    @data.tables << table_obj
     return self
   end
 
@@ -95,35 +82,35 @@ class SqlStmt
   end
 
   def where(*expr)
-    @wheres.concat(expr)
+    @data.wheres.concat(expr)
     return self
   end
 
   def no_where
-    @where_behavior = :exclude
+    @data.where_behavior = :exclude
     return self
   end
 
   def optional_where
-    @where_behavior = :optional
+    @data.where_behavior = :optional
     return self
   end
 
   def get(*exprs)
-    @fields.concat(exprs)
-    @called_get = true
+    @data.fields.concat(exprs)
+    @data.called_get = true
     return self
   end
 
   def set(field, value)
-    raise "trying to include field #{field} again" if @fields.include?(field)
+    raise "trying to include field #{field} again" if @data.fields.include?(field)
     # this is to support the special case of INSERT INTO table SELECT * FROM ...
     # where * specified with no matching insert field list specified
     if field
-      @fields << field
+      @data.fields << field
     end
     value = value.is_a?(String) ? value : value.to_sql
-    @values << value
+    @data.values << value
     return self
   end
 
@@ -132,84 +119,84 @@ class SqlStmt
   end
 
   def group_by(expr)
-    @group_by = expr
+    @data.group_by = expr
     return self
   end
 
   def order_by(expr)
-    @order_by = expr
+    @data.order_by = expr
     return self
   end
 
   def limit(clause)
-    @limit = clause
+    @data.limit = clause
     return self
   end
 
   def having(*expr)
-    @having.concat(expr)
+    @data.having.concat(expr)
     return self
   end
 
   def with_rollup
-    @with_rollup = true
+    @data.with_rollup = true
     return self
   end
 
   # used with INSERT statements only
   def into(into_table)
-    @into_table = into_table
+    @data.into_table = into_table
     return self
   end
 
   # used with INSERT VALUES statements only
   def add_row(row)
-    @rows << row
+    @data.rows << row
   end
 
   ###### less commonly used methods
 
   def distinct
-    @distinct = true
+    @data.distinct = true
     return self
   end
 
   def straight_join
-    @straight_join = true
+    @data.straight_join = true
     return self
   end
 
   def replace
-    @replace = true
+    @data.replace = true
     return self
   end
 
   def ignore
-    @ignore = 'IGNORE '
+    @data.ignore = 'IGNORE '
     return self
   end
 
   def outfile(str)
-    @outfile = " INTO OUTFILE #{str}"
+    @data.outfile = " INTO OUTFILE #{str}"
     return self
   end
 
   ###### methods to analyze what the statement contains
   def includes_table?(table_to_find)
-    retval = @tables.find { |table| (table.name == table_to_find) || (table.alias == table_to_find) }
-    retval ||= @joins.find { |_, table, _| table_names_match?(table, table_to_find) }
+    retval = @data.tables.find { |table| (table.name == table_to_find) || (table.alias == table_to_find) }
+    retval ||= @data.joins.find { |_, table, _| table_names_match?(table, table_to_find) }
     return retval
   end
 
 private
   def ensure_no_statement_type
-    if @stmt_type
-      raise "statement type already set to #{@stmt_type}"
+    if @data.stmt_type
+      raise "statement type already set to #{@data.stmt_type}"
     end
   end
 
   def add_join(keyword, table, exprs)
-    @joins << [keyword, table, "ON #{exprs.join(' AND ')}"]
+    @data.joins << [keyword, table, "ON #{exprs.join(' AND ')}"]
     return self
   end
 
