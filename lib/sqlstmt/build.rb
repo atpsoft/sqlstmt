269a30e09783012eee28fa2983f4d13135cfa8b9
Makani Mason <gitauthor@pqmland.com>
2018-08-02 09:40:10 -0600

    refactor out MysqlChecker

--- lib/sqlstmt/build.rb
+++ lib/sqlstmt/build.rb
@@ -3,6 +3,50 @@ require 'sqlstmt/error'
 module SqlStmtLib
 extend self
 
+class MysqlChecker
+  def initialize(data)
+    @data = data
+  end
+
+  def run
+    if !@data.stmt_type
+      raise SqlStmtError, "unable to build sql - must call :select, :update, :insert or :delete to specify statement type"
+    end
+    if @data.tables.empty?
+      raise SqlStmtError, "unable to build sql - must call :table"
+    end
+
+    if (@data.where_behavior == :require) && @data.wheres.empty?
+      raise SqlStmtError, "unable to build sql - must call :where, :no_where, or :optional_where"
+    elsif (@data.where_behavior == :exclude) && !@data.wheres.empty?
+      raise SqlStmtError, "unable to build sql - :where and :no_where must not both be called, consider :optional_where instead"
+    end
+
+    if @data.stmt_type == 'select'
+      raise SqlStmtError, "unable to build sql - must call :get" if @data.fields.empty?
+      raise SqlStmtError, "unable to build sql - must not call :set" if !@data.values.empty?
+    else
+      raise SqlStmtError, "unable to build sql - must not call :get" if @data.called_get
+    end
+
+    if ['update','insert'].include?(@data.stmt_type)
+      raise SqlStmtError, "unable to build sql - must call :set or :setq" if @data.values.empty?
+      raise SqlStmtError, "unable to build sql - must not call :get" if @data.called_get
+    end
+
+    if @data.stmt_type == 'insert'
+      raise SqlStmtError, "unable to build sql - must call :into" if @data.into_table.nil?
+    end
+
+    if @data.stmt_type == 'delete'
+      raise SqlStmtError, "unable to build sql - must not call :get or :set" if !@data.fields.empty?
+      if @data.tables_to_delete.empty? && ((@data.tables.size + @data.joins.size) > 1)
+        raise SqlStmtError, "unable to build sql - must specify tables to delete when including multiple tables"
+      end
+    end
+  end
+end
+
 class MysqlBuilder
   def initialize(data)
     @data = data
@@ -108,49 +152,8 @@ end
 
 class SqlStmt
   def to_s
-    verify_minimum_requirements
+    SqlStmtLib::MysqlChecker.new(@data).run
     return SqlStmtLib::MysqlBuilder.new(@data).build_stmt
   end
   alias_method :to_sql, :to_s
-
-  ###### the remainder of the methods are for verifying and building the completed statement string
-
-  def verify_minimum_requirements
-    if !@data.stmt_type
-      raise SqlStmtError, "unable to build sql - must call :select, :update, :insert or :delete to specify statement type"
-    end
-    if @data.tables.empty?
-      raise SqlStmtError, "unable to build sql - must call :table"
-    end
-
-    if (@data.where_behavior == :require) && @data.wheres.empty?
-      raise SqlStmtError, "unable to build sql - must call :where, :no_where, or :optional_where"
-    elsif (@data.where_behavior == :exclude) && !@data.wheres.empty?
-      raise SqlStmtError, "unable to build sql - :where and :no_where must not both be called, consider :optional_where instead"
-    end
-
-    if @data.stmt_type == 'select'
-      raise SqlStmtError, "unable to build sql - must call :get" if @data.fields.empty?
-      raise SqlStmtError, "unable to build sql - must not call :set" if !@data.values.empty?
-    else
-      raise SqlStmtError, "unable to build sql - must not call :get" if @data.called_get
-    end
-
-    if ['update','insert'].include?(@data.stmt_type)
-      raise SqlStmtError, "unable to build sql - must call :set or :setq" if @data.values.empty?
-      raise SqlStmtError, "unable to build sql - must not call :get" if @data.called_get
-    end
-
-    if @data.stmt_type == 'insert'
-      raise SqlStmtError, "unable to build sql - must call :into" if @data.into_table.nil?
-    end
-
-    if @data.stmt_type == 'delete'
-      raise SqlStmtError, "unable to build sql - must not call :get or :set" if !@data.fields.empty?
-      if @data.tables_to_delete.empty? && ((@data.tables.size + @data.joins.size) > 1)
-        raise SqlStmtError, "unable to build sql - must specify tables to delete when including multiple tables"
-      end
-    end
-  end
-
 end
