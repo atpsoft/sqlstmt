e162dca6670d4b9baecb977c9ed568cb82ee51da
pquimo <pquimo@users.noreply.github.com>
2022-02-10 11:15:03 -0800

    update BigDecimal creation

--- test/to_sql_test.rb
+++ test/to_sql_test.rb
@@ -10,7 +10,7 @@ class Test_to_sql < Minitest::Test
     assert_equal("'b\\\"lah'", "b\"lah".to_sql())
 
     assert_equal('3', 3.to_sql)
-    assert_equal('10.0', BigDecimal.new('10').to_sql)
+    assert_equal('10.0', BigDecimal('10').to_sql)
 
     assert_equal('1', true.to_sql)
     assert_equal('0', false.to_sql)
